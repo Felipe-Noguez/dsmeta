caae5c1a54edc18f0126727468865c30be418fbd
Felipe-Noguez <felipenoguez@outlook.com.br>
2022-09-11 18:37:52 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
   return (
-    <h1>Hello!</h1>
+    <>
+      <h1>Hello!</h1>
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
